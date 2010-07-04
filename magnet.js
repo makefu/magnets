9dfb63470d4a233de0cf7e4e1331fde24dc8b025
Sven Pfleiderer <sg+github@roothausen.de>
2010-07-04 04:38:45 +0200

    tried to add html-xml parsing, failed
    we tried to parse the html page via node-xml library but parsing failed
    due to fucking javascript !!!!
    
    we try regexp in the next commit

--- magnet.js
+++ magnet.js
@@ -4,7 +4,47 @@ var http = require('http'),
 fs = require('fs'),
 sys = require('sys'),
 url = require('url'),
-rss = require('./lib/node-xml');
+xml = require('./lib/node-xml');
+
+
+function getImages(content, callback) {
+  var parser = new xml.SaxParser(function(cb) {
+    var images = new Array();
+
+    cb.onStartDocument(function() { });
+
+    cb.onEndDocument(function() {     
+      callback(images);
+    });
+
+    cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
+      current_element = elem.toLowerCase();
+      sys.puts(current_element)
+      if(current_element == 'img') {
+        sys.puts(attrs)
+        //images.push(attrs["src"]) 
+      }
+    });
+
+    cb.onEndElementNS(function(elem, prefix, uri) {
+    });
+
+    cb.onCharacters(function (chars) { });
+    cb.onCdata(function(chars) { } );
+
+    cb.onWarning(function(msg) {
+      sys.puts('<WARNING>'+msg+"</WARNING>");
+    });
+
+    cb.onError(function(msg) {
+      sys.puts('<ERROR>'+JSON.stringify(msg)+"</ERROR>");
+    });
+
+  });
+
+  parser.parseString(content)
+}
+
 
 function httpGet(myUrl,crawl) {
   var parsedUrl = url.parse(myUrl)
@@ -19,21 +59,24 @@ function httpGet(myUrl,crawl) {
       responseBody += chunk
     });
     response.addListener("end", function() {
-      sys.puts("end!");
       crawl(responseBody)
     });
   });
   request.end();
 }
 
-function crawlerfunct(xml) {
-  sys.puts(xml)
+function crawlerfunct(content) {
+  getImages(content, function(images) {
+    images.forEach(function(image) {
+      sys.puts(image);
+    }); 
+  });
 }
 
 try {
   sys.puts("Start crawling");
   var crawlUrl = "http://www.soup.io/everyone"
   httpGet(crawlUrl,crawlerfunct)
-} catch(ex) {
+  } catch(ex) {
     sys.puts("Error: " + puts);
-}
+  }
