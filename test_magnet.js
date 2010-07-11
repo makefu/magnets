91d6cd26bbb39af56a27b74cd58958dbac2d38e0
Sven Pfleiderer <sg+github@roothausen.de>
2010-07-11 20:21:09 +0200

    Moved magnets.js to lib/magnetlib.js, fixed some formatting issues

--- test_magnet.js
+++ test_magnet.js
@@ -1,6 +1,9 @@
-var mag = require('./magnets'),
+// soup.io module
+
+var mag = require('./lib/magnetlib'),
     sys = require('sys'),
     url = require('url');
+
 var log = mag.log;
 var TIMEOUT = 3000;
 var MAINPAGE="http://www.soup.io/everyone";
@@ -15,7 +18,6 @@ function getImages(content) {
   var imageFilter = /<img .* src=['"]{1}([\S]*)['"]{1}\s?(.*)\/>/g;
   var refFilter = /<a .*href=['"]{1}([\S]*)['"]{1}\s?class="lightbox"(.*)>/g;
   var images = new Array();
-  var match;
 
   //TODO: REFACTOR ME!
   while (match = imageFilter.exec(content)) {
@@ -26,13 +28,25 @@ function getImages(content) {
       }
     }
   }
-  while (match = refFilter.exec(content)) {
+
+  var refImages  = getMatches(refFilter, content);
+
+  return refImages.concat(images);
+
+}
+
+function getMatches(regex, content) {
+  var matches = new Array();
+
+  var match; 
+  while (match = regex.exec(content)) {
     if(match != null && match != undefined) {
-      log.debug("found: "+ match[0])
-      images.push(match[1]);
+      log.debug("found: "+ match[0]);
+      matches.push(match[1]);
     }
   }
-  return images;
+  return matches;
+
 }
 
 /*
@@ -47,11 +61,11 @@ function crawl(crawlUrl  ) {
 
 function crawlBack(cUrl) {
     cUrl = cUrl || "http://www.soup.io/everyone";
-    log.info("crawling" + cUrl);
-    crawl(cUrl);
-    getNextUrl(cUrl)
-    
+    log.info("crawling " + cUrl);
+//    crawl(cUrl);
+    getNextUrl(cUrl);
 }
+
 function getNextUrl(cUrl) {
     //TODO refactor me ( do not want to request webpage again ...
     log.debug('trying to get data from :'+cUrl);
@@ -62,21 +76,22 @@ function getNextUrl(cUrl) {
             }
         });
 }
+
 function parseNextUrl(content) {
     urlPattern = /SOUP.Endless.next_url = \'([\S]*)\'/ ;
-    match = urlPattern.exec(content.data)
+    match = urlPattern.exec(content.data);
+
     if(match != null && match != undefined) {
         log.debug("next url is: "+ match[1]);
         var parsedUrl = url.parse(content.url);
-
         return parsedUrl.protocol+'//'+parsedUrl.host+match[1];
-    }else {
+    } else {
         // this should never happen
         // means we reached the end of the soup.io
         log.warn("Didn't find next url, assumed we reached the end of soup.io");
 
         return undefined;
     }
-
 }
+
 crawlBack ();
