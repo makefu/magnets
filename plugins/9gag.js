f938dfe016ed843a4ebfcfcb7d6c84dd1a1cefb7
Sven Pfleiderer <sg+github@roothausen.de>
2010-08-15 05:29:09 +0200

    Added 9gag module skeleton

---
+++ plugins/9gag.js
@@ -0,0 +1,31 @@
+/*
+ * magnets.js module for http://9gag.com
+ */
+
+var MAIN = "http://9gag.com/"
+
+exports.LIVE =  MAIN
+
+/// begin of crawling for crawling backwards
+exports.BACKWARDS = MAIN 
+exports.NAME = "9gag Plugin"
+
+/* returns all funny pictures from 9gag frontpage
+ *
+ * @param content of webpage
+ * @return array of urls
+ */
+exports.getImages = function getImages(content) { 
+    return [];
+};
+
+/* returns the next URL to parse backwards
+ *
+ * The next url should be the link to the 'next' page in the timeline
+ * @param the content of the page
+ * @return the url string 
+ */
+
+exports.getNextUrl = function getNextUrl(content) {
+  return MAIN;
+};
