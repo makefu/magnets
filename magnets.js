1c73ab0d29b49ea8fad49ed447d70370181b0aca
Sven Pfleiderer <sg+github@roothausen.de>
2010-08-15 03:42:15 +0200

    Did some refactoring
    Did some minor refactoring, added new jsdoc comments and method
    descriptions

--- magnets.js
+++ magnets.js
@@ -1,13 +1,24 @@
-/* This is a prototype entrypoint for our module system */
+/**
+ * This is the main scheduler for magnets.js
+ * 
+ * It's main purpose is to manage and schedule
+ * modules for different websites
+ *
+ * @author makefu
+ * @author pfleidi
+ *
+ */
 
 var mag = require('./lib/magnetlib'),
-    fs = require('fs'),
-    sys = require('sys');
+  fs = require('fs'),
+  sys = require('sys');
 
 var log = mag.log;
-var modules = new Array();
-var TIMEOUT = 10000
+var modules = [];
+var TIMEOUT = 10000;
+
 /* Process Logging */
+
 process.on('uncaughtException', function (err) {
   sys.puts('Caught exception: ' + err);
   log.fatal('Caught exception: ' + err);
@@ -20,15 +31,27 @@ process.on('SIGINT', function () {
 
 process.on('exit', function () {
   log.info('Exited successfully ...');
+  process.exit(0);
 });
 
+/**
+ * @param   {String} fileName
+ * @return  {String} 
+ *
+ */
 
 function getModuleName(fileName) {
   return fileName.split('\.')[0];
 }
 
+/**
+ * Scans the ./plugins/ directory for modules
+ *
+ * all modules are stored inside modules
+ */
+
 function initModules() {
-  modules = new Array(); 
+  modules = [];
   fs.readdir('./plugins/', function(err, files) {
     if (err) {
       log.warn('Error while reading files: ' + err);
@@ -44,31 +67,40 @@ function initModules() {
   });
 };
 
-/** @brief runs a live
-  *
-  * schedules a Live module, will schedule self after TIMEOUT
-  *
-  * @author   makefu
-  * @date     2010-08-01  
-  * @param    mod  the module loded by require()
-  */
+/** 
+ * runs a crawl for given module
+ *
+ * schedules a Live module
+ *
+ * @param    mod  the module loded by require()
+ */
+
 function runLiveMod(mod){
   var img = new mag.Image(mod.LIVE)
-  mag.httpGet(img ,function(content) {
+  mag.httpGet(img, function(content) {
     var images = mod.getImages(content);
     mag.downloadImages(images);
   });
 }
 
+/**
+ * Schedules live crawls for all available modules
+ *
+ * All modules are re-scheduled after TIMEOUT
+ *
+ */
+
 function runLiveModules() {
-  log.info("Running live modules")
-  var currTimeout=0;
+  log.info("Running live modules");
+  var currTimeout = 0;
+  
   modules.forEach(function(mod) {
-    log.debug("starting module: "+mod.NAME + " at Timeout "+currTimeout);
-    setTimeout(function () { runLiveMod(mod)},currTimeout); 
-    currTimeout = currTimeout + TIMEOUT
+    log.debug("starting module: " + mod.NAME + " at Timeout " + currTimeout);
+    setTimeout(function () { runLiveMod(mod) }, currTimeout); 
+    currTimeout = currTimeout + TIMEOUT;
   });
-  setTimeout(runLiveModules,currTimeout); 
+
+  setTimeout(runLiveModules, currTimeout); 
 }
 
 function main() {
